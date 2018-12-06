0affcf6a5fc9f9838071ee49a51c5ec114703521
Kyrosu <nils.hoogakker@hotmail.nl>
2018-12-06 15:35:06 +0100

    test2

--- src/displayVeld.java
+++ src/displayVeld.java
@@ -5,9 +5,9 @@ import javax.swing.JTextPane;
 public class displayVeld extends JTextPane {
 	
 	public displayVeld (int x, int y, int width, int height) {
-		this.setBounds(x, y, width, height);
-		this.setEditable(false);
-		this.setBackground(Color.white);
+	//	this.setBounds(x, y, width, height);
+	//	this.setEditable(false);
+		//pthis.setBackground(Color.white);
 
 	}
 }
